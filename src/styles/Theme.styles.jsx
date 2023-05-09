7a0baaa93f2692e1f55f11136722fc17f1db6381
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-08 21:51:53 -0300

    style: adicionado arquivo responsável pelos temas padrões da aplicação já com os valores de cores

---
+++ src/styles/Theme.styles.jsx
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: '#357EDB',
+    secondaryLight: '#5299F2',
+    secondaryDark: '#0054BF',
+    contrastLight: '#FFA500',
+    contrastDark: '#C05F06',
+    neutralWith: '#D9EBEE',
+    neutralBlack: '#0F0E0E',
+    error: '#FFEF15',
+  },
+};
+
+export default theme;
