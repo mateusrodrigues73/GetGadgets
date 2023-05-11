af9d8b9b731926a3a72a23673107a29d06b7c76e
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-11 10:41:49 -0300

    feat: adcionar o resto dos componentes no Header junto com o tratamento do campo SearchInput

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,65 @@
+import { useState } from 'react';
+
 import {
   HeaderContainer,
+  HeaderWrapper,
   Logo,
   SearchInput,
   SearchLinksContainer,
   SearchContainer,
   SearchIcon,
   LinksContainer,
+  LinkIconContainer,
   LinkItem,
+  UserIcon,
+  ShoppingCarIcon,
+  LinkWithIcon,
 } from './Header.styles';
 
 function Header() {
+  // eslint-disable-next-line no-unused-vars
+  const [searchInput, setSearchinput] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchinput(event.target.value);
+  };
+
+  const findProducts = () => {
+    // TODO: implementar função para busca de produtos
+  };
+
   return (
     <HeaderContainer>
-      <Logo src="/images/logo.png" />
-      <SearchLinksContainer>
-        <SearchContainer>
-          <SearchInput placeholder="busque aqui o seu produto" />
-          <SearchIcon />
-        </SearchContainer>
-        <LinksContainer>
-          <LinkItem to="/">Fóruns</LinkItem>
-          <LinkItem to="/">Seu histórico</LinkItem>
-          <LinkItem to="/">Busca avançada</LinkItem>
-          <LinkItem to="/">Seus anúncios</LinkItem>
-        </LinksContainer>
-      </SearchLinksContainer>
+      <HeaderWrapper>
+        <Logo src="/images/logo.png" />
+        <SearchLinksContainer>
+          <SearchContainer>
+            <SearchInput
+              placeholder="busque aqui o seu produto"
+              onChange={handleInputChange}
+            />
+            <SearchIcon onClick={findProducts} />
+          </SearchContainer>
+          <LinksContainer>
+            <LinkItem to="/">Fóruns</LinkItem>
+            <LinkItem to="/">Seu histórico</LinkItem>
+            <LinkItem to="/">Busca avançada</LinkItem>
+            <LinkItem to="/">Seus anúncios</LinkItem>
+          </LinksContainer>
+        </SearchLinksContainer>
+        <LinkIconContainer>
+          <LinkWithIcon to="/">
+            Faça seu login <br /> ou cadastre-se
+          </LinkWithIcon>
+          <UserIcon />
+        </LinkIconContainer>
+        <LinkIconContainer>
+          <LinkWithIcon to="/">
+            Seu carrinho <br /> de compras
+          </LinkWithIcon>
+          <ShoppingCarIcon />
+        </LinkIconContainer>
+      </HeaderWrapper>
     </HeaderContainer>
   );
 }
