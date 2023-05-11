c67f017ca2367e9b5293824ab83f50b2ecfec3c3
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-10 23:58:35 -0300

    feat: adicionar logo, searhInput e links no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,32 @@
+import {
+  HeaderContainer,
+  Logo,
+  SearchInput,
+  SearchLinksContainer,
+  SearchContainer,
+  SearchIcon,
+  LinksContainer,
+  LinkItem,
+} from './Header.styles';
+
 function Header() {
-  return <div />;
+  return (
+    <HeaderContainer>
+      <Logo src="/images/logo.png" />
+      <SearchLinksContainer>
+        <SearchContainer>
+          <SearchInput placeholder="busque aqui o seu produto" />
+          <SearchIcon />
+        </SearchContainer>
+        <LinksContainer>
+          <LinkItem to="/">Fóruns</LinkItem>
+          <LinkItem to="/">Seu histórico</LinkItem>
+          <LinkItem to="/">Busca avançada</LinkItem>
+          <LinkItem to="/">Seus anúncios</LinkItem>
+        </LinksContainer>
+      </SearchLinksContainer>
+    </HeaderContainer>
+  );
 }
 
 export default Header;
