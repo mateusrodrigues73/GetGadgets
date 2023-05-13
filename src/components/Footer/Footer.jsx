ed16dc4d47e651f2dd8827923f1d278d5ba39c11
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 09:41:44 -0300

    feat: adicionar seções de links no rodapé

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,56 @@
 import {
   FooterContainer,
+  FooterWrapper,
   SectionTitle,
   SocialContainer,
   SocialIconsContainer,
   FacebookIcon,
   InstagramIcon,
   TwitterIcon,
+  LinksContainer,
+  LinksWrapper,
+  LinksColumn,
+  FooterLink,
 } from './Footer.styles';
 
 const Footer = () => (
   <FooterContainer>
-    <SocialContainer>
-      <SectionTitle>
-        Acompanhe nossas redes <br /> sociais
-      </SectionTitle>
-      <SocialIconsContainer>
-        <FacebookIcon />
-        <InstagramIcon />
-        <TwitterIcon />
-      </SocialIconsContainer>
-    </SocialContainer>
+    <FooterWrapper>
+      <SocialContainer>
+        <SectionTitle>
+          Acompanhe nossas redes <br /> sociais
+        </SectionTitle>
+        <SocialIconsContainer>
+          <FacebookIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+        </SocialIconsContainer>
+      </SocialContainer>
+      <LinksContainer>
+        <SectionTitle>Navegue pelo site</SectionTitle>
+        <LinksWrapper>
+          <LinksColumn>
+            <FooterLink to="/">seus anúncios</FooterLink>
+            <FooterLink to="/">seus histórico</FooterLink>
+            <FooterLink to="/">fóruns</FooterLink>
+          </LinksColumn>
+          <LinksColumn>
+            <FooterLink to="/">busca avançada</FooterLink>
+            <FooterLink to="/">
+              seu carrinho <br /> de compras
+            </FooterLink>
+          </LinksColumn>
+        </LinksWrapper>
+      </LinksContainer>
+      <LinksContainer>
+        <SectionTitle>Termo e políticas</SectionTitle>
+        <LinksColumn>
+          <FooterLink to="/">termos de publicação</FooterLink>
+          <FooterLink to="/">termos de serviço</FooterLink>
+          <FooterLink to="/">política de privacidade</FooterLink>
+        </LinksColumn>
+      </LinksContainer>
+    </FooterWrapper>
   </FooterContainer>
 );
 
