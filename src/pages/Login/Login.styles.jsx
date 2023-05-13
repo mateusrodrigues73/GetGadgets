52463263b85f0e367de7e4d386223f64c829bbe3
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 12:01:51 -0300

    style: card da página Login

---
+++ src/pages/Login/Login.styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.div`
+  margin: 0 auto;
+  width: 286px;
+  background-color: ${({ theme }) => theme.colors.secondaryDark};
+  border-radius: 30px 0px;
+  box-shadow: 8px 8px 4px 1px rgba(15, 14, 14, 0.5);
+  margin-bottom: 9%;
+  padding: 33px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoginWrapper = styled.div``;
