7eb3ddd3eb0f36f9bd6402d4208a50ee6740f412
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 18:14:50 -0300

    style: finalizar estilização do componente GradientButton

--- src/components/GradientButton/GradientButton.styles.jsx
+++ src/components/GradientButton/GradientButton.styles.jsx
@@ -1,9 +1,28 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  background-color: pink;
   width: ${({ width }) => width};
   height: ${({ height }) => height};
+  background: linear-gradient(
+    90deg,
+    ${({ theme }) => theme.colors.contrastLight},
+    ${({ theme }) => theme.colors.contrastDark}
+  );
+  border: none;
+  border-radius: 10px 0px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.heading};
+
+  &:hover {
+    cursor: pointer;
+    font-size: 20px;
+    outline: 2px solid ${({ theme }) => theme.colors.neutralWith};
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.contrastDark};
+  }
 `;
 
 export default Button;
