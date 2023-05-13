5656424f6c8618770d5e4ea5c717eee0a4b582f5
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 16:29:06 -0300

    feat: adicionar corpo do componente GradientButton

--- src/components/GradientButton/GradientButton.jsx
+++ src/components/GradientButton/GradientButton.jsx
@@ -1,3 +1,14 @@
-const GradientButton = () => <div />;
+import PropTypes from 'prop-types';
+
+import Button from './GradientButton.styles';
+
+const GradientButton = ({ width, height }) => (
+  <Button width={width} heigth={height} />
+);
+
+GradientButton.propTypes = {
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+};
 
 export default GradientButton;
