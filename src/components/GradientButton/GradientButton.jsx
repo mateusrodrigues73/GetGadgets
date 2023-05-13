018e44592ff9df51f355de25fba86df7a78f0513
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 18:14:25 -0300

    feat: configurar props do componente GradientButton

--- src/components/GradientButton/GradientButton.jsx
+++ src/components/GradientButton/GradientButton.jsx
@@ -2,13 +2,17 @@ import PropTypes from 'prop-types';
 
 import Button from './GradientButton.styles';
 
-const GradientButton = ({ width, height }) => (
-  <Button width={width} heigth={height} />
+const GradientButton = ({ width, height, text, onClick }) => (
+  <Button width={width} height={height} onClick={onClick}>
+    {text}
+  </Button>
 );
 
 GradientButton.propTypes = {
   width: PropTypes.string.isRequired,
   height: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default GradientButton;
