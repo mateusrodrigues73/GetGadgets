f8c57b0d821dfc5f66969a2156cd0f5de31ca563
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 13:04:14 -0300

    feat: importar componente AuthInput

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,11 @@
 import { LoginContainer } from './Login.styles';
 import AuthTitle from '../../components/AuthTitle';
+import AuthInput from '../../components/AuthInput';
 
 const Login = () => (
   <LoginContainer>
     <AuthTitle title="Fazer login" />
+    <AuthInput placeholder="E-mail" />
   </LoginContainer>
 );
 
