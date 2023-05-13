c969c8e3f8be9ea99cb684b5869ce043274dd716
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 14:22:36 -0300

    feat: adicionar checkbox e link de recuperação de senha

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,26 @@
-import { LoginContainer } from './Login.styles';
+import {
+  LoginContainer,
+  CheckBoxContainer,
+  Checkbox,
+  CheckBoxText,
+  ForgotPassContainer,
+} from './Login.styles';
 import AuthTitle from '../../components/AuthTitle';
 import AuthInput from '../../components/AuthInput';
+import AuthLink from '../../components/AuthLink';
 
 const Login = () => (
   <LoginContainer>
     <AuthTitle title="Fazer login" />
     <AuthInput placeholder="E-mail" />
+    <AuthInput placeholder="Senha" />
+    <CheckBoxContainer>
+      <Checkbox type="checkbox" />
+      <CheckBoxText>Lembre-me</CheckBoxText>
+    </CheckBoxContainer>
+    <ForgotPassContainer>
+      <AuthLink to="/" text="Esqueceu sua senha?" />
+    </ForgotPassContainer>
   </LoginContainer>
 );
 
