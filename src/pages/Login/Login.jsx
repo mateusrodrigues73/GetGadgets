3d96ada987099022cea13d63850221e8b5b5d81e
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-13 18:13:31 -0300

    feat: adicionar estrutura da função logar e o resto das propiedades do GradintButton

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,20 +10,31 @@ import AuthInput from '../../components/AuthInput';
 import AuthLink from '../../components/AuthLink';
 import GradientButton from '../../components/GradientButton';
 
-const Login = () => (
-  <LoginContainer>
-    <AuthTitle title="Fazer login" />
-    <AuthInput placeholder="E-mail" />
-    <AuthInput placeholder="Senha" />
-    <CheckBoxContainer>
-      <Checkbox type="checkbox" />
-      <CheckBoxText>Lembre-me</CheckBoxText>
-    </CheckBoxContainer>
-    <ForgotPassContainer>
-      <AuthLink to="/" text="Esqueceu sua senha?" />
-    </ForgotPassContainer>
-    <GradientButton width="220px" height="25px" />
-  </LoginContainer>
-);
+const Login = () => {
+  const logar = () => {
+    // TODO: implementar função para logar usuário
+  };
+
+  return (
+    <LoginContainer>
+      <AuthTitle title="Fazer login" />
+      <AuthInput placeholder="E-mail" />
+      <AuthInput placeholder="Senha" />
+      <CheckBoxContainer>
+        <Checkbox type="checkbox" />
+        <CheckBoxText>Lembre-me</CheckBoxText>
+      </CheckBoxContainer>
+      <ForgotPassContainer>
+        <AuthLink to="/" text="Esqueceu sua senha?" />
+      </ForgotPassContainer>
+      <GradientButton
+        width="220px"
+        height="25px"
+        text="Entrar"
+        onClick={logar}
+      />
+    </LoginContainer>
+  );
+};
 
 export default Login;
