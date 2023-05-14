ceaefb5533a93faaebf0c6d3ae325602ad576cd6
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-14 10:40:49 -0300

    feat: adicionar rota de recuperação de senha

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import App from './App';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import ForgotPassWord from './pages/ForgotPassword';
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
       {
         path: '/cadastro',
         element: <SignUp />,
+      },
+      {
+        path: '/recuperar-senha',
+        element: <ForgotPassWord />,
       },
     ],
   },
