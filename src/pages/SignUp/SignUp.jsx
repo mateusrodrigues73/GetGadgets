30bbb7721dbff9d5720dfc4100131eddc9928f57
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-14 10:05:06 -0300

    feat: estrutura principal da view do componente

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,33 @@
-const SignUp = () => (
-  <div>
-    <h1>Cadastre-se</h1>
-  </div>
-);
+import { SignUpContainer, SignUpWrapper } from './SIgnUp.styles';
+import AuthTitle from '../../components/AuthTitle';
+import AuthInput from '../../components/AuthInput';
+import AuthLink from '../../components/AuthLink';
+import GradientButton from '../../components/GradientButton';
+
+const SignUp = () => {
+  const logar = () => {
+    // TODO: implementar função de cadastrar usuário
+  };
+
+  return (
+    <SignUpContainer>
+      <AuthTitle title="Cadastre-se" />
+      <AuthInput placeholder="Nome" />
+      <AuthInput placeholder="Sobrenome" />
+      <AuthInput placeholder="E-mail" />
+      <AuthInput placeholder="Senha" />
+      <AuthInput placeholder="Confirmação de senha" />
+      <GradientButton
+        width="220px"
+        height="25px"
+        text="Cadastrar"
+        onClick={logar}
+      />
+      <SignUpWrapper>
+        <AuthLink to="/entrar" text="Voltar" />
+      </SignUpWrapper>
+    </SignUpContainer>
+  );
+};
 
 export default SignUp;
