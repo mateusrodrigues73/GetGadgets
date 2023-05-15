36d506bad3441fed9e879287fe210d3d745372b8
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-15 16:00:04 -0300

    feat: adicionar função para pegar o valor do input

--- src/components/AuthInput/AuthInput.jsx
+++ src/components/AuthInput/AuthInput.jsx
@@ -2,10 +2,17 @@ import PropTypes from 'prop-types';
 
 import Input from './AuthInput.styles';
 
-const AuthInput = ({ placeholder }) => <Input placeholder={placeholder} />;
+const AuthInput = ({ placeholder, setValue }) => {
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return <Input placeholder={placeholder} onChange={handleInputChange} />;
+};
 
 AuthInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  setValue: PropTypes.func.isRequired,
 };
 
 export default AuthInput;
