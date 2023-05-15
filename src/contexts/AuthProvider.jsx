5dbc7580f3139876687b51e080c1c19edf3bedb4
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-15 20:41:52 -0300

    feat: criar função para logar usuário

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,18 +1,49 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { createClient } from '@supabase/supabase-js';
+
+import showToast from '../utils/ShowToasts';
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [session, setSession] = useState(null);
+  const navigate = useNavigate();
+
+  const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+  );
+
+  const signIn = async (email, password) => {
+    let msg = '';
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) {
+        throw new Error(error.message);
+      } else {
+        setSession(data);
+        msg = 'Login bem sucedido';
+        showToast('sign-in-success', 'success', msg);
+        navigate('/');
+      }
+    } catch (error) {
+      if (error.message === 'Invalid login credentials') {
+        msg = 'Credenciais de login inválidas!';
+      }
+      showToast('sign-in-error', 'error', msg);
+    }
+    return msg;
+  };
 
   const authContextValue = {
     session,
+    signIn,
   };
-
-  useEffect(() => {
-    setSession('sessão');
-  }, []);
 
   return (
     <AuthContext.Provider value={authContextValue}>
