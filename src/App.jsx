328ac4a84d4d698e522a1bfa5fcff38da3bb2648
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-15 20:38:25 -0300

    feat: adicionar componente de mensagens a árvore de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
+import Toast from './styles/Toast';
+
 import { AuthProvider } from './contexts/AuthProvider';
 
 import Header from './components/Header';
@@ -7,6 +9,7 @@ import Footer from './components/Footer';
 
 const App = () => (
   <AuthProvider>
+    <Toast />
     <Header />
     <Outlet />
     <Footer />
