e01aa718c3cb0e36711866b5c59033488230dd7d
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-15 10:45:47 -0300

    feat: adicionar Authprovider na árvore de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import { AuthProvider } from './contexts/AuthProvider';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => (
-  <>
+  <AuthProvider>
     <Header />
     <Outlet />
     <Footer />
-  </>
+  </AuthProvider>
 );
 
 export default App;
