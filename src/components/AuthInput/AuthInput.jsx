0677c89b87357d4f670582081748f3fecfd52449
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-16 11:06:09 -0300

    feat: adicionar prop opcional para padding

--- src/components/AuthInput/AuthInput.jsx
+++ src/components/AuthInput/AuthInput.jsx
@@ -2,20 +2,30 @@ import PropTypes from 'prop-types';
 
 import Input from './AuthInput.styles';
 
-const AuthInput = ({ placeholder, type, setValue }) => {
+const AuthInput = ({ placeholder, type, padding, setValue }) => {
   const handleInputChange = (event) => {
     setValue(event.target.value);
   };
 
   return (
-    <Input placeholder={placeholder} type={type} onChange={handleInputChange} />
+    <Input
+      placeholder={placeholder}
+      type={type}
+      onChange={handleInputChange}
+      padding={padding}
+    />
   );
 };
 
 AuthInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  padding: PropTypes.string,
   setValue: PropTypes.func.isRequired,
+};
+
+AuthInput.defaultProps = {
+  padding: '',
 };
 
 export default AuthInput;
