439afcb4bcb0741e811ba869e37e983f5bfc9b28
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-16 08:38:48 -0300

    feat: adicionar prop para o tipo do input

--- src/components/AuthInput/AuthInput.jsx
+++ src/components/AuthInput/AuthInput.jsx
@@ -2,16 +2,19 @@ import PropTypes from 'prop-types';
 
 import Input from './AuthInput.styles';
 
-const AuthInput = ({ placeholder, setValue }) => {
+const AuthInput = ({ placeholder, type, setValue }) => {
   const handleInputChange = (event) => {
     setValue(event.target.value);
   };
 
-  return <Input placeholder={placeholder} onChange={handleInputChange} />;
+  return (
+    <Input placeholder={placeholder} type={type} onChange={handleInputChange} />
+  );
 };
 
 AuthInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
   setValue: PropTypes.func.isRequired,
 };
 
