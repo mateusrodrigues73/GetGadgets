421fbff6374a1101f203e1ac0956e2d58b343e61
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-16 09:24:44 -0300

    feat: finalizar componente AuthPassInput

--- src/components/AuthPassInput/AuthPassInput.jsx
+++ src/components/AuthPassInput/AuthPassInput.jsx
@@ -1,7 +1,43 @@
-const AuthPassInput = () => (
-  <div>
-    <input />
-  </div>
-);
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { PassContainer, ShowIcon, HiddenIcon } from './AuthPassInput.styles';
+
+import AuthInput from '../AuthInput';
+
+const AuthPassInput = ({ placeholder, setValue }) => {
+  const [hidden, setHidden] = useState(true);
+  const [type, setType] = useState('password');
+
+  const setInput = () => {
+    if (hidden) {
+      setHidden(false);
+    } else {
+      setHidden(true);
+    }
+
+    if (type === 'password') {
+      setType('text');
+    } else {
+      setType('password');
+    }
+  };
+
+  return (
+    <PassContainer>
+      <AuthInput placeholder={placeholder} type={type} setValue={setValue} />
+      {hidden ? (
+        <HiddenIcon onClick={setInput} />
+      ) : (
+        <ShowIcon onClick={setInput} />
+      )}
+    </PassContainer>
+  );
+};
+
+AuthPassInput.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  setValue: PropTypes.func.isRequired,
+};
 
 export default AuthPassInput;
