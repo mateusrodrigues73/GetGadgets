f61cd0a0d49cbb5b5c89fabdb34bcb99f99e657b
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-16 09:24:10 -0300

    style: definir estilos do componente AuthPassInput

---
+++ src/components/AuthPassInput/AuthPassInput.styles.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
+
+export const PassContainer = styled.div`
+  position: relative;
+`;
+
+export const HiddenIcon = styled(EyeInvisibleOutlined)`
+  position: absolute;
+  font-size: 16px;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  color: ${({ theme }) => theme.colors.neutralWith};
+
+  &:hover {
+    cursor: pointer;
+    font-size: 19px;
+  }
+`;
+
+export const ShowIcon = styled(EyeOutlined)`
+  position: absolute;
+  font-size: 16px;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  color: ${({ theme }) => theme.colors.neutralWith};
+
+  &:hover {
+    cursor: pointer;
+    font-size: 19px;
+  }
+`;
