50b316e501c0bc819e3709c385e9fff247d50fd6
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-17 20:12:04 -0300

    fix: corrigir método para acessar as váriaveis de ambiente

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -12,8 +12,8 @@ export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
 
   const supabase = createClient(
-    process.env.REACT_APP_SUPABASE_URL,
-    process.env.REACT_APP_SUPABASE_ANON_KEY
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
   );
 
   const signIn = async (email, password) => {
