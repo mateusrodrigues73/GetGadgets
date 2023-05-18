ead0c7542c9529b619ad60fbbf686c6c3e975e6a
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-18 09:58:17 -0300

    refactor: remover prop padding

--- src/components/AuthInput/AuthInput.jsx
+++ src/components/AuthInput/AuthInput.jsx
@@ -2,30 +2,21 @@ import PropTypes from 'prop-types';
 
 import Input from './AuthInput.styles';
 
-const AuthInput = ({ placeholder, type, padding, setValue }) => {
+const AuthInput = ({ placeholder, type, setValue }) => {
   const handleInputChange = (event) => {
     setValue(event.target.value);
   };
 
   return (
-    <Input
-      placeholder={placeholder}
-      type={type}
-      onChange={handleInputChange}
-      padding={padding}
-    />
+    <Input placeholder={placeholder} type={type} onChange={handleInputChange} />
   );
 };
 
 AuthInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
-  padding: PropTypes.string,
-  setValue: PropTypes.func.isRequired,
-};
 
-AuthInput.defaultProps = {
-  padding: '',
+  setValue: PropTypes.func.isRequired,
 };
 
 export default AuthInput;
