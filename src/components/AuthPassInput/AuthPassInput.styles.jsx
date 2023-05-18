d9dff6795b2151818898c578e182cbf105c2f161
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-18 10:00:48 -0300

    style: estilizar componente input

--- src/components/AuthPassInput/AuthPassInput.styles.jsx
+++ src/components/AuthPassInput/AuthPassInput.styles.jsx
@@ -34,3 +34,43 @@ export const ShowIcon = styled(EyeOutlined)`
     color: ${({ theme }) => theme.colors.neutralWith};
   }
 `;
+
+export const Input = styled.input`
+  width: 220px;
+  height: 30px;
+  padding-left: 12px;
+  padding-right: 35px;
+  background-color: ${({ theme }) => theme.colors.secondaryDark};
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutralWith};
+  outline: none;
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+
+  &::placeholder {
+    font-family: ${({ theme }) => theme.fonts.body};
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.neutralWith};
+  }
+
+  &:hover {
+    border-bottom-color: ${({ theme }) => theme.colors.secondaryLight};
+    color: ${({ theme }) => theme.colors.secondaryLight};
+
+    &::placeholder {
+      color: ${({ theme }) => theme.colors.secondaryLight};
+    }
+  }
+
+  &:focus {
+    border: none;
+    border-radius: 10px 0px;
+    background-color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.neutralWith};
+
+    &::placeholder {
+      color: ${({ theme }) => theme.colors.neutralWith};
+    }
+  }
+`;
