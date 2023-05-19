fdb4b63e9333bd411fc69a61c4b32c71c7addf29
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-19 11:29:38 -0300

    feat: adicionar estilos para instruções de senha

--- src/pages/SignUp/SIgnUp.styles.jsx
+++ src/pages/SignUp/SIgnUp.styles.jsx
@@ -13,6 +13,7 @@ export const SignUpContainer = styled.div`
   align-items: center;
   justify-content: center;
   row-gap: 1.5rem;
+  position: relative;
 `;
 
 export const SignUpWrapper = styled.div`
@@ -21,4 +22,26 @@ export const SignUpWrapper = styled.div`
   flex-direction: row;
   align-self: flex-start;
   padding-left: 12px;
+`;
+
+export const PassInstructionsWrapper = styled.div`
+  width: 260px;
+  background-color: ${({ theme }) => theme.colors.secondaryDark};
+  border-radius: 30px 0px;
+  padding-left: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: absolute;
+  left: 300px;
+  top: 190px;
+`;
+
+export const PassInstructions = styled.span`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+  line-height: 1.5;
 `;
