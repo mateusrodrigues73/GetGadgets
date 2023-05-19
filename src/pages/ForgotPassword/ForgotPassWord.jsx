ee1d7da0ca43716e4e731ac6c408b72ff854190e
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-19 11:26:57 -0300

    feat: adicionar state de e-mail

--- src/pages/ForgotPassword/ForgotPassWord.jsx
+++ src/pages/ForgotPassword/ForgotPassWord.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
+
 import {
   ForgotPassContainer,
   ForgotPassWrapper,
   Message,
 } from './ForgotPassword.styles';
+
 import AuthTitle from '../../components/AuthTitle';
 import AuthInput from '../../components/AuthInput';
 import AuthLink from '../../components/AuthLink';
 import GradientButton from '../../components/GradientButton';
 
 const ForgotPassWord = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [email, setEmail] = useState('');
+
   const recuperar = () => {
     // TODO: implementar função para enviar e-mail de confirmação de senha
   };
@@ -19,7 +25,7 @@ const ForgotPassWord = () => {
       <Message>
         Enviaremos as instruções para o seu e-mail já cadastrado
       </Message>
-      <AuthInput placeholder="Seu e-mail" />
+      <AuthInput placeholder="E-mail" type="email" setValue={setEmail} />
       <GradientButton
         width="220px"
         height="25px"
