d038570a8cab2192d8d089a1a677e2cf6bb9b33a
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-22 09:29:46 -0300

    feat: adicionar Supabaseprovider na árvore de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { Outlet } from 'react-router-dom';
 
 import Toast from './styles/Toast';
 
+import { SupabaseProvider } from './contexts/SupabaseProvider';
 import { AuthProvider } from './contexts/AuthProvider';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => (
-  <AuthProvider>
-    <Toast />
-    <Header />
-    <Outlet />
-    <Footer />
-  </AuthProvider>
+  <SupabaseProvider>
+    <AuthProvider>
+      <Toast />
+      <Header />
+      <Outlet />
+      <Footer />
+    </AuthProvider>
+  </SupabaseProvider>
 );
 
 export default App;
