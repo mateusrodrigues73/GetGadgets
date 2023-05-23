7ff3095288de747bd07b05d4f5a07d7ddd0e1403
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-23 11:06:10 -0300

    chore: remover comentário de linha

--- src/components/Loader/Loader.styles.jsx
+++ src/components/Loader/Loader.styles.jsx
@@ -6,7 +6,7 @@ const LoaderContainer = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* Cor de fundo com transparência */
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
