c4e3b072db840b2ede2dfa65141cde57f3300266
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-29 11:18:54 -0300

    feat: verificar evento de recuperação de senha

--- src/pages/UpdatePassword/UpdatePassword.jsx
+++ src/pages/UpdatePassword/UpdatePassword.jsx
@@ -1,3 +1,19 @@
-const UpdatePassword = () => <div />;
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { AuthContext } from '../../contexts/AuthProvider';
+
+const UpdatePassword = () => {
+  const { recoveryPass } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!recoveryPass) {
+      navigate('/');
+    }
+  }, []);
+
+  return <div>recovery</div>;
+};
 
 export default UpdatePassword;
