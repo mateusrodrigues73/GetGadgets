5f8741fc17d7fbbe7444bc229f3a12a7346e1626
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-29 11:16:26 -0300

    feat: botão de logout provisório para testes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,23 @@
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+import { useContext } from 'react';
+
+import GradientButton from '../../components/GradientButton';
+
+import { AuthContext } from '../../contexts/AuthProvider';
+
+const Home = () => {
+  const { signOut } = useContext(AuthContext);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <GradientButton
+        width="220px"
+        height="25px"
+        text="Logout"
+        onClick={signOut}
+      />
+    </div>
+  );
+};
 
 export default Home;
