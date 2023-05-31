aee147fea395c5dce878c5def3cb9b0372a290a5
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-31 11:12:31 -0300

    feat: finalizar desenvolvimento página de erro

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,25 @@
-const ErrorPage = () => <h1>404</h1>;
+import {
+  ErrorContainer,
+  ErrorWrapper,
+  ErrorTitle,
+  ErrorMessage,
+} from './ErroPage.styles';
+
+import AuthLink from '../../components/AuthLink';
+
+const ErrorPage = () => (
+  <ErrorContainer>
+    <ErrorWrapper>
+      <ErrorTitle>Ops! Página não encontrada</ErrorTitle>
+      <ErrorMessage>
+        Parece que não encontramos a página que você está procurando
+      </ErrorMessage>
+      <ErrorMessage>
+        Verifique se a URL está correta ou retorne à página inicial.
+      </ErrorMessage>
+      <AuthLink to="/" text="Ir para Home" />
+    </ErrorWrapper>
+  </ErrorContainer>
+);
 
 export default ErrorPage;
