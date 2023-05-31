e64476e6243a9c24d87425a9c178ba694e530cd7
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-05-31 10:02:40 -0300

    feat: adicionar página de erro nas rotas da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import App from './App';
+
 import GlobalStyle from './styles/Global';
 import Theme from './styles/Theme';
-import App from './App';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import ForgotPassWord from './pages/ForgotPassword';
 import UpdatePassword from './pages/UpdatePassword';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/',
