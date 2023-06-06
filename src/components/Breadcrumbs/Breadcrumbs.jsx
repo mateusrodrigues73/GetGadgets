61fdb46b0878ee65b1311ffe521f55c85d101f3b
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-06 11:03:45 -0300

    feeat: tratar rotas recebidas (incompleto)

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,3 +1,29 @@
-const Breadcrumbs = () => <div />;
+import PropTypes from 'prop-types';
+
+import { LinksContainer, Links } from './Breadcrumbs.styles';
+
+const Breadcrumbs = ({ linksString }) => {
+  const pairsLinks = linksString.split('|');
+  const links = [];
+
+  pairsLinks.forEach((pair) => {
+    const [key, value] = pair.split('\\');
+    links.push({ key, value });
+  });
+
+  return (
+    <LinksContainer>
+      {links.map(({ key, value }) => (
+        <Links key={key} to={key}>
+          {value}
+        </Links>
+      ))}
+    </LinksContainer>
+  );
+};
+
+Breadcrumbs.propTypes = {
+  linksString: PropTypes.string.isRequired,
+};
 
 export default Breadcrumbs;
