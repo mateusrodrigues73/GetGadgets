e4553c933cda475ae506b7abebd0de657003dfa2
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-11 18:11:19 -0300

    feat: adicionar icone e botão de fto de perfil

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,8 +1,41 @@
+import {
+  ProfileContainer,
+  UserIconContainer,
+  UserIconWrapper,
+  UserIcon,
+} from './UserProfile.styles';
+
 import Breadcrumbs from '../../components/Breadcrumbs';
+import SectionTitle from '../../components/SectionTitle';
+import GradientButton from '../../components/GradientButton';
 
 const UserProfile = () => {
-  const string = '/\\Home|/perfil\\Perfil|/cadastro\\Cadastre-se';
-  return <Breadcrumbs linksString={string} />;
+  const string = '/\\Home';
+
+  const changePicture = () => {
+    // TODO: implementar função para alterar foto do usuário
+  };
+
+  return (
+    <>
+      <Breadcrumbs linksString={string} actualPage="Perfil" />
+      <SectionTitle title="Seus dados" />
+      <ProfileContainer>
+        <UserIconContainer>
+          <UserIconWrapper>
+            <UserIcon />
+          </UserIconWrapper>
+          <GradientButton
+            width="170px"
+            height="25px"
+            text="Enviar foto"
+            onClick={changePicture}
+          />
+        </UserIconContainer>
+      </ProfileContainer>
+      <SectionTitle title="Seus endereços" />
+    </>
+  );
 };
 
 export default UserProfile;
