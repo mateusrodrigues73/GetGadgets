a4b72628c69944bda878a87a9e5c2763d229459f
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-11 17:38:29 -0300

    feat: finalizar componente SectionTitle

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,3 +1,17 @@
-const SectionTitle = () => <div />;
+import PropTypes from 'prop-types';
+
+import { TitleContainer, TitleWrapper, Title } from './SectionTitle.styles';
+
+const SectionTitle = ({ title }) => (
+  <TitleContainer>
+    <TitleWrapper>
+      <Title>{title}</Title>
+    </TitleWrapper>
+  </TitleContainer>
+);
+
+SectionTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default SectionTitle;
