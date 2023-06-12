3ecd71539d124828f76ca74438539b1aa6303a30
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-12 10:11:27 -0300

    feat: adicionar inputs e botões para edição do perfil

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -3,17 +3,34 @@ import {
   UserIconContainer,
   UserIconWrapper,
   UserIcon,
+  InputContainer,
+  InputName,
+  Input,
+  EditIcon,
 } from './UserProfile.styles';
 
 import Breadcrumbs from '../../components/Breadcrumbs';
 import SectionTitle from '../../components/SectionTitle';
 import GradientButton from '../../components/GradientButton';
+import CautionButton from '../../components/CautionButton';
 
 const UserProfile = () => {
   const string = '/\\Home';
 
   const changePicture = () => {
     // TODO: implementar função para alterar foto do usuário
+  };
+
+  const changePassword = () => {
+    // TODO: implementar função para alterar senha do usuário
+  };
+
+  const deleteProfile = () => {
+    // TODO: implementar função para deletar perfil do usuário
+  };
+
+  const logOut = () => {
+    // TODO: implementar função para deslogar usuário
   };
 
   return (
@@ -32,6 +49,41 @@ const UserProfile = () => {
             onClick={changePicture}
           />
         </UserIconContainer>
+        <InputContainer>
+          <InputName>Nome:</InputName>
+          <Input />
+          <EditIcon />
+        </InputContainer>
+        <InputContainer>
+          <InputName>Sobrenome:</InputName>
+          <Input />
+          <EditIcon />
+        </InputContainer>
+        <InputContainer>
+          <InputName>E-mail:</InputName>
+          <Input />
+          <EditIcon />
+        </InputContainer>
+        <GradientButton
+          width="377px"
+          height="25px"
+          text="Alterar senha"
+          onClick={changePassword}
+        />
+        <CautionButton
+          width="377px"
+          height="25px"
+          text="Sair"
+          onClick={logOut}
+          icon={false}
+        />
+        <CautionButton
+          width="377px"
+          height="25px"
+          text="Deletar conta"
+          onClick={deleteProfile}
+          icon
+        />
       </ProfileContainer>
       <SectionTitle title="Seus endereços" />
     </>
