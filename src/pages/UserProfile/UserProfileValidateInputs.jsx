e74c7dca5e9925ddf3d28c31aec960045a0fdba3
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-12 12:03:43 -0300

    feat: adicionar arquivo de validação do perfil

---
+++ src/pages/UserProfile/UserProfileValidateInputs.jsx
@@ -0,0 +1,43 @@
+const validate = (nome, sobrenome, setMessage, setMessageId) => {
+  const nomeRegex = /^[A-Za-z]{3,}(?:\s[A-Za-z]{3,})*$/;
+
+  if (nome === '') {
+    setMessageId('cadastrar-fields-validate-warn');
+    setMessage('Campo nome não pode estar vazio!');
+    return false;
+  }
+
+  if (sobrenome === '') {
+    setMessageId('cadastrar-fields-validate-warn');
+    setMessage('Campo sobrenome não pode estar vazio!');
+    return false;
+  }
+
+  if (nome.length >= 30) {
+    setMessageId('nome-validate-length-warn');
+    setMessage('Nome deve ter menos de 30 caracteres!');
+    return false;
+  }
+
+  if (!nomeRegex.test(nome)) {
+    setMessageId('nome-validate-format-warn');
+    setMessage('Nome possui formato Inválido!');
+    return false;
+  }
+
+  if (sobrenome.length >= 50) {
+    setMessageId('sobrenome-validate-length-warn');
+    setMessage('Sobrenome deve ter menos de 50 caracteres!');
+    return false;
+  }
+
+  if (!nomeRegex.test(sobrenome)) {
+    setMessageId('sobrenome-validate-format-warn');
+    setMessage('Sobrenomeome possui formato Inválido!');
+    return false;
+  }
+
+  return true;
+};
+
+export default validate;
