6844531d50f1d092b068b17b5f8fa6f7623f2ae7
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-06-19 10:25:02 -0300

    feat: finalizar componente Alert

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,3 +1,54 @@
-const Alert = () => <div />;
+import PropTypes from 'prop-types';
+
+import {
+  AlertContainer,
+  AlertWrapper,
+  Message,
+  ButtonsContainer,
+} from './Alert.styles';
+
+import AuthTitle from '../AuthTitle';
+import GradientButton from '../GradientButton';
+import CautionButton from '../CautionButton';
+
+const Alert = ({ message, onCancel, onContinue }) => {
+  const cancel = () => {
+    onCancel();
+  };
+
+  const proceed = () => {
+    onContinue();
+  };
+
+  return (
+    <AlertContainer>
+      <AlertWrapper>
+        <AuthTitle title="Atenção!" />
+        <Message>{message}</Message>
+        <ButtonsContainer>
+          <GradientButton
+            width="150px"
+            height="25px"
+            text="Cancelar"
+            onClick={cancel}
+          />
+          <CautionButton
+            width="150px"
+            height="25px"
+            text="Continuar"
+            onClick={proceed}
+            icon
+          />
+        </ButtonsContainer>
+      </AlertWrapper>
+    </AlertContainer>
+  );
+};
+
+Alert.propTypes = {
+  message: PropTypes.string.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onContinue: PropTypes.func.isRequired,
+};
 
 export default Alert;
