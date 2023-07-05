559dca6898346fc9370b7362e1de705f3df9d3a8
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-07-05 10:51:35 -0300

    feat: adicicionar input de categorias (incompleto)

--- src/pages/ProductAdPosting/ProductAdPosting.jsx
+++ src/pages/ProductAdPosting/ProductAdPosting.jsx
@@ -1,3 +1,19 @@
-const ProductAdPosting = () => <div />;
+import { useState } from 'react';
+
+import { PostingContainer, Title, Input } from './ProductAdPosting.styles';
+
+const ProductAdPosting = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [categoria, setCategoria] = useState('Selecione uma categoria');
+
+  const changeCategory = () => {};
+
+  return (
+    <PostingContainer>
+      <Title>Selecione a categoria do produto</Title>
+      <Input type="text" defaultValue={categoria} onClick={changeCategory} />
+    </PostingContainer>
+  );
+};
 
 export default ProductAdPosting;
