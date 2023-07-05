673a6f730ffef2a21465d0b0c464105d603411e3
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-07-05 10:49:58 -0300

    feat: adicionar array de categorias

--- src/contexts/ProductProvider.jsx
+++ src/contexts/ProductProvider.jsx
@@ -4,7 +4,34 @@ import PropTypes from 'prop-types';
 export const ProductContext = createContext({});
 
 export const ProductProvider = ({ children }) => {
-  const productContextValue = {};
+  const categorias = [
+    {
+      categoria: 'Hardware',
+      descricao: 'Placa de vídeo, memórias, processador...',
+    },
+    {
+      categoria: 'Periféricos',
+      descricao: 'Teclado, mouse, headset...',
+    },
+    {
+      categoria: 'Computadores',
+      descricao: 'PC, notebook, monitores...',
+    },
+    {
+      categoria: 'Celular & Smartphone',
+      descricao: 'Celulares e smartphones',
+    },
+    {
+      categoria: 'TV',
+      descricao: 'Smart Tv, acessórios para TV',
+    },
+    {
+      categoria: 'Games',
+      descricao: 'Consoles, controle, jogos em midia fisica...',
+    },
+  ];
+
+  const productContextValue = { categorias };
 
   return (
     <ProductContext.Provider value={{ productContextValue }}>
