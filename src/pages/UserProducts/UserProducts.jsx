41bb17209925d219e4bedfa206f2f6b083b121ec
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-07-05 09:31:06 -0300

    feat: adicionar botão para criar anúncio (temporário)

--- src/pages/UserProducts/UserProducts.jsx
+++ src/pages/UserProducts/UserProducts.jsx
@@ -1,3 +1,26 @@
-const UserProducts = () => <div />;
+import { useNavigate } from 'react-router-dom';
+
+import Container from './Userproducts.styles';
+
+import GradientButton from '../../components/GradientButton';
+
+const UserProducts = () => {
+  const navigate = useNavigate();
+
+  const criarAnuncio = () => {
+    navigate('/cadastrar-anuncio');
+  };
+
+  return (
+    <Container>
+      <GradientButton
+        width="220px"
+        height="25px"
+        text="Cadastrar anúncio"
+        onClick={criarAnuncio}
+      />
+    </Container>
+  );
+};
 
 export default UserProducts;
---
+++ src/pages/UserProducts/Userproducts.styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 1200px;
+  height: ${window.innerHeight - 300}px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export default Container;
