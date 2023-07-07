14df0676173c7816ab59d6f34f55735b7ecf5d88
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-07-07 09:41:50 -0300

    feat: adicionar escolha de categoria do produto

--- src/pages/ProductAdPosting/ProductAdPosting.jsx
+++ src/pages/ProductAdPosting/ProductAdPosting.jsx
@@ -1,18 +1,63 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
-import { PostingContainer, Title, Input } from './ProductAdPosting.styles';
+import {
+  PostingContainer,
+  Title,
+  SelectInput,
+  CategoriesPickerContainer,
+  CategoriesPickerWrapper,
+} from './ProductAdPosting.styles';
+
+import Breadcrumbs from '../../components/Breadcrumbs';
+
+import { ProductContext } from '../../contexts/ProductProvider';
+// import { AuthContext } from '../../contexts/AuthProvider';
 
 const ProductAdPosting = () => {
-  // eslint-disable-next-line no-unused-vars
   const [categoria, setCategoria] = useState('Selecione uma categoria');
+  const [pickCategories, setPickCategories] = useState(false);
+  const { categorias } = useContext(ProductContext);
+  // const { sessionUser } = useContext(AuthContext);
+  const linksString = '/\\Home|/seus-anuncios\\Seus anúncios';
 
-  const changeCategory = () => {};
+  const changeCategory = () => {
+    setPickCategories(true);
+  };
+
+  const chooseCategory = (category) => {
+    setCategoria(`${category.categoria}: ${category.descricao}`);
+    setPickCategories(false);
+  };
+
+  const showCategories = () => (
+    <CategoriesPickerContainer>
+      <CategoriesPickerWrapper>
+        {categorias.map((category) => (
+          <SelectInput
+            key={category.categoria}
+            type="text"
+            defaultValue={`${category.categoria}: ${category.descricao}`}
+            onClick={() => chooseCategory(category)}
+          />
+        ))}
+      </CategoriesPickerWrapper>
+    </CategoriesPickerContainer>
+  );
 
   return (
-    <PostingContainer>
-      <Title>Selecione a categoria do produto</Title>
-      <Input type="text" defaultValue={categoria} onClick={changeCategory} />
-    </PostingContainer>
+    <>
+      <Breadcrumbs linksString={linksString} actualPage="Cadastrar anúncio" />
+      <PostingContainer>
+        <Title>Selecione a categoria do produto</Title>
+        <SelectInput
+          type="text"
+          readOnly
+          value={categoria}
+          onClick={changeCategory}
+        />
+      </PostingContainer>
+      {pickCategories && showCategories()}
+    </>
   );
 };
 
