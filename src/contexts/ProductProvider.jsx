670a021671acd22a1efc8dc0e5eb8cd4ee97a165
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-07-08 17:04:47 -0300

    feat: adicionar função getLastProducts

--- src/contexts/ProductProvider.jsx
+++ src/contexts/ProductProvider.jsx
@@ -1,9 +1,19 @@
-import { createContext } from 'react';
+import { useState, createContext, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import PreLoader from '../components/PreLoader';
+
+import { SupabaseContext } from './SupabaseProvider';
+import { AuthContext } from './AuthProvider';
 
 export const ProductContext = createContext({});
 
 export const ProductProvider = ({ children }) => {
+  const [lastProducts, setLastproducts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { supabase } = useContext(SupabaseContext);
+  const { sessionUser } = useContext(AuthContext);
+
   const categorias = [
     {
       categoria: 'Hardware',
@@ -31,11 +41,46 @@ export const ProductProvider = ({ children }) => {
     },
   ];
 
-  const productContextValue = { categorias };
+  const getLastProducts = async () => {
+    setLoading(true);
+    try {
+      if (sessionUser) {
+        const { data, error } = await supabase
+          .from('produtos')
+          .select('*, produto_imagens(*)')
+          .neq('id_usuario', sessionUser.id)
+          .order('created_at', { ascending: false })
+          .limit(10);
+        setLastproducts(error ? null : data);
+        if (error) {
+          throw new Error(error);
+        }
+      } else {
+        const { data, error } = await supabase
+          .from('produtos')
+          .select('*, produto_imagens(*)')
+          .order('created_at', { ascending: false })
+          .limit(10);
+        setLastproducts(error ? null : data);
+        if (error) {
+          throw new Error(error);
+        }
+      }
+    } catch (error) {
+      return;
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getLastProducts();
+  }, [sessionUser]);
+
+  const productContextValue = { categorias, lastProducts };
 
   return (
     <ProductContext.Provider value={productContextValue}>
-      {children}
+      {loading ? <PreLoader /> : children}
     </ProductContext.Provider>
   );
 };
