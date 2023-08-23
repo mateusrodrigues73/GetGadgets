2e9b85acdbc3789851849b0def395951b2f33bef
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-08-22 21:59:17 -0300

    feat: adicionar mensagem de ajuda de categorias

--- src/contexts/ProductProvider.jsx
+++ src/contexts/ProductProvider.jsx
@@ -41,6 +41,9 @@ export const ProductProvider = ({ children }) => {
     },
   ];
 
+  const categoryMessageTip =
+    'Seja bem-vindo à nossa plataforma de venda de produtos eletrônicos. Antes de anunciar o seu produto, é importante lembrar que trabalhamos apenas com produtos que se enquadram em nossas categorias. Certifique-se de que o seu produto está dentro de uma dessas categorias antes de continuar com o anúncio. Para garantir a qualidade e a segurança de nossos usuários, nos reservamos o direito de remover qualquer anúncio impróprio ou que não se enquadre em nossas regras. Agradecemos a sua compreensão e colaboração para mantermos um ambiente saudável e seguro para todos os usuários.';
+
   const getLastProducts = async () => {
     setLoading(true);
     try {
@@ -76,7 +79,7 @@ export const ProductProvider = ({ children }) => {
     getLastProducts();
   }, [sessionUser]);
 
-  const productContextValue = { categorias, lastProducts };
+  const productContextValue = { categorias, lastProducts, categoryMessageTip };
 
   return (
     <ProductContext.Provider value={productContextValue}>
