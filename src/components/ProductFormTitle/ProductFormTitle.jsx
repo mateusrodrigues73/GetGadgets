843d470e8b42591d3fdf33acd16321a3d4b215a9
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-08-23 22:04:11 -0300

    feat: finalizar seção de escolha do título

--- src/components/ProductFormTitle/ProductFormTitle.jsx
+++ src/components/ProductFormTitle/ProductFormTitle.jsx
@@ -1,25 +1,82 @@
+import { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import {
   PostingContainer,
   TitleContainer,
   Title,
+  Input,
+  LinkButtonContainer,
 } from './ProductFormTitle.styles';
 
 import ProgressBar from '../ProgressBar';
 import HelpIcon from '../HelpIcon';
+import OrangeButton from '../OrangeButton';
 
-// TODO: fixar eslint-disable-next-line
-// eslint-disable-next-line no-unused-vars
-const ProductFormTitle = ({ currentStep, totalSteps, setActualStep }) => (
-  <PostingContainer>
-    <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
-    <TitleContainer>
-      <Title>Selecione o título do anúncio</Title>
-      <HelpIcon message="123" />
-    </TitleContainer>
-  </PostingContainer>
-);
+import { titleMessageTip } from '../../data/adPostingMessagesTips';
+
+import showToast from '../../utils/showToasts';
+
+import { ProductContext } from '../../contexts/ProductProvider';
+
+const ProductFormTitle = ({ currentStep, totalSteps, setActualStep }) => {
+  const [isValid, setIsvalid] = useState(false);
+  const [title, setTitle] = useState('');
+  const { posting } = useContext(ProductContext);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const previous = () => {
+    posting.title = title;
+    setActualStep(currentStep - 1);
+  };
+
+  const next = () => {
+    if (isValid) {
+      posting.title = title;
+      setActualStep(currentStep + 1);
+    } else {
+      showToast('product-form-title-warn', 'error', 'Título inválido!');
+    }
+  };
+
+  useEffect(() => {
+    const regex = /^(?!.* {2})(?=.*\S)[\s\S]{10,80}$/;
+    if (regex.test(title)) {
+      setIsvalid(true);
+    } else {
+      setIsvalid(false);
+    }
+  }, [title]);
+
+  useEffect(() => {
+    if (posting.title) {
+      setTitle(posting.title);
+    }
+  }, []);
+
+  return (
+    <PostingContainer>
+      <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
+      <TitleContainer>
+        <Title>Selecione o título do anúncio</Title>
+        <HelpIcon message={titleMessageTip} />
+      </TitleContainer>
+      <Input
+        type="text"
+        placeholder="Título"
+        value={title}
+        onChange={handleTitleChange}
+      />
+      <LinkButtonContainer>
+        <OrangeButton action={previous} text="Anterior" />
+        <OrangeButton action={next} text="Próximo" />
+      </LinkButtonContainer>
+    </PostingContainer>
+  );
+};
 
 ProductFormTitle.propTypes = {
   currentStep: PropTypes.number.isRequired,
--- src/components/ProductFormTitle/ProductFormTitle.styles.jsx
+++ src/components/ProductFormTitle/ProductFormTitle.styles.jsx
@@ -17,11 +17,49 @@ export const PostingContainer = styled.div`
 export const TitleContainer = styled.div`
   display: flex;
   flex-direction: row;
-  column-gap: 15px;
+  column-gap: 10px;
 `;
 
 export const Title = styled.h1`
   font-family: ${({ theme }) => theme.fonts.heading};
   font-size: 20px;
   color: ${({ theme }) => theme.colors.neutralWith};
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 30px;
+  background-color: ${({ theme }) => theme.colors.secondaryLight};
+  border-radius: 10px 0px;
+  padding-left: 12px;
+  border: none;
+  outline: none;
+  font-family: ${({ theme }) => theme.fonts.heading};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary};
+    cursor: pointer;
+  }
+
+  &:focus {
+    background-color: ${({ theme }) => theme.colors.secondaryLight};
+    border: 1px solid ${({ theme }) => theme.colors.neutralWith};
+  }
+
+  &::placeholder {
+    font-family: ${({ theme }) => theme.fonts.heading};
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.neutralWith};
+  }
+`;
+
+export const LinkButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-right: 30px;
+  padding-left: 30px;
 `;
