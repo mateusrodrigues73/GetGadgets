44736a965cdbe0a6f79cacab404c63d1b0f49a48
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-08-23 22:01:40 -0300

    feat: adicionar mensagem de ajuda para o título

--- src/data/adPostingMessagesTips.js
+++ src/data/adPostingMessagesTips.js
@@ -1,4 +1,13 @@
-const categoryMessageTip =
+export const categoryMessageTip =
   'Seja bem-vindo à nossa plataforma de venda de produtos eletrônicos. Antes de anunciar o seu produto, é importante lembrar que trabalhamos apenas com produtos que se enquadram em nossas categorias. Certifique-se de que o seu produto está dentro de uma dessas categorias antes de continuar com o anúncio. Para garantir a qualidade e a segurança de nossos usuários, nos reservamos o direito de remover qualquer anúncio impróprio ou que não se enquadre em nossas regras. Agradecemos a sua compreensão e colaboração para mantermos um ambiente saudável e seguro para todos os usuários.';
 
-export default categoryMessageTip;
+export const titleMessageTip = `
+  O título do seu anúncio é a primeira impressão que os compradores terão do seu produto. Ele deve ser claro e atrativo.
+  Aqui estão algumas dicas e informações úteis:
+  - O título deve ter entre 10 e 80 caracteres.
+  - Evite usar mais de dois espaços em branco consecutivos.
+  - Certifique-se de que o título descreve com precisão o que você está vendendo.
+  - Use palavras-chave relevantes para tornar seu anúncio fácil de encontrar.
+  - Seja honesto e evite usar informações enganosas.
+  Exemplo de Título Adequado: "(tipo do produto, ex.: Smart TV) (nome da marca) (nome do modelo) (alguma característica relevante, ex.: 64 polegadas)"
+`;
