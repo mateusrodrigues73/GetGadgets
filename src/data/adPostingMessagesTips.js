8de4e1f5a1e942c01d289d59d27a6f5af3536091
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-08-27 10:19:51 -0300

    feat: adicionar mensagem de ajuda para especificações

--- src/data/adPostingMessagesTips.js
+++ src/data/adPostingMessagesTips.js
@@ -11,3 +11,12 @@ export const titleMessageTip = `
   - Seja honesto e evite usar informações enganosas.
   Exemplo de Título Adequado: "(tipo do produto, ex.: Smart TV) (nome da marca) (nome do modelo) (alguma característica relevante, ex.: 64 polegadas)"
 `;
+
+export const specsMessageTip = `
+  Nesta seção, você pode fornecer detalhes específicos sobre o seu produto. Quanto mais informações precisas você fornecer, mais confiança isso irá gerar nos compradores.
+  Aqui estão algumas dicas para incluir nas especificações:
+  - Detalhes técnicos relevantes.
+  - Condição do produto (novo, usado, recondicionado, etc.).
+  - Inclua todos os acessórios ou itens adicionais incluídos.
+  - Seja transparente sobre qualquer defeito ou desgaste.
+`;
