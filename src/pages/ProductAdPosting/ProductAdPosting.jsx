2be52b5731f3f82c1075c4ab6e1f317c6b5a2eaa
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-09-01 10:47:08 -0300

    feat: adicionar componente ProductFormCover

--- src/pages/ProductAdPosting/ProductAdPosting.jsx
+++ src/pages/ProductAdPosting/ProductAdPosting.jsx
@@ -4,6 +4,7 @@ import ProductFormCategory from '../../components/ProductFormCategory';
 import ProductFormTitle from '../../components/ProductFormTitle';
 import ProductFormData from '../../components/ProductFormData';
 import ProductFormSpecs from '../../components/ProductFormSpecs';
+import ProductFormCover from '../../components/ProductFormCover';
 
 import Breadcrumbs from '../../components/Breadcrumbs';
 
@@ -46,6 +47,15 @@ const ProductAdPosting = () => {
       return (
         <ProductFormSpecs
           currentStep={4}
+          totalSteps={6}
+          setActualStep={setActualStep}
+        />
+      );
+    }
+    if (actualStep === 5) {
+      return (
+        <ProductFormCover
+          currentStep={5}
           totalSteps={6}
           setActualStep={setActualStep}
         />
