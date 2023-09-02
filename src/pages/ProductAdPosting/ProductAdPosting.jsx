2c7248bc0bceff28b0f3250891020ca75d886231
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-09-02 11:02:22 -0300

    refactor: criar array com componentes do formulário

--- src/pages/ProductAdPosting/ProductAdPosting.jsx
+++ src/pages/ProductAdPosting/ProductAdPosting.jsx
@@ -10,68 +10,43 @@ import ProductFormImages from '../../components/ProductFormImages';
 import Breadcrumbs from '../../components/Breadcrumbs';
 
 const ProductAdPosting = () => {
-  // const { sessionUser } = useContext(AuthContext);
   const [actualStep, setActualStep] = useState(1);
   const linksString = '/\\Home|/seus-anuncios\\Seus anúncios';
 
-  // TODO: fixar eslint-disable-next-line
-  // eslint-disable-next-line consistent-return
-  const showForm = () => {
-    if (actualStep === 1) {
-      return (
-        <ProductFormCategory
-          currentStep={1}
-          totalSteps={6}
-          setActualStep={setActualStep}
-        />
-      );
-    }
-    if (actualStep === 2) {
-      return (
-        <ProductFormTitle
-          currentStep={2}
-          totalSteps={6}
-          setActualStep={setActualStep}
-        />
-      );
-    }
-    if (actualStep === 3) {
-      return (
-        <ProductFormData
-          currentStep={3}
-          totalSteps={6}
-          setActualStep={setActualStep}
-        />
-      );
-    }
-    if (actualStep === 4) {
-      return (
-        <ProductFormSpecs
-          currentStep={4}
-          totalSteps={6}
-          setActualStep={setActualStep}
-        />
-      );
-    }
-    if (actualStep === 5) {
-      return (
-        <ProductFormCover
-          currentStep={5}
-          totalSteps={6}
-          setActualStep={setActualStep}
-        />
-      );
-    }
-    if (actualStep === 6) {
-      return (
-        <ProductFormImages
-          currentStep={6}
-          totalSteps={6}
-          setActualStep={setActualStep}
-        />
-      );
-    }
-  };
+  const formSteps = [
+    <ProductFormCategory
+      currentStep={1}
+      totalSteps={6}
+      setActualStep={setActualStep}
+    />,
+    <ProductFormTitle
+      currentStep={2}
+      totalSteps={6}
+      setActualStep={setActualStep}
+    />,
+    <ProductFormData
+      currentStep={3}
+      totalSteps={6}
+      setActualStep={setActualStep}
+    />,
+    <ProductFormSpecs
+      currentStep={4}
+      totalSteps={6}
+      setActualStep={setActualStep}
+    />,
+    <ProductFormCover
+      currentStep={5}
+      totalSteps={6}
+      setActualStep={setActualStep}
+    />,
+    <ProductFormImages
+      currentStep={6}
+      totalSteps={6}
+      setActualStep={setActualStep}
+    />,
+  ];
+
+  const showForm = () => formSteps[actualStep - 1];
 
   return (
     <>
