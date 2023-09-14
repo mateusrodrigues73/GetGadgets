b7ea67cc067468f2d229db7bb022786b2f4e10d1
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-09-14 14:49:28 -0300

    styles: finalizar estilos da página de anúncios do usuário

--- src/pages/UserProducts/Userproducts.styles.jsx
+++ src/pages/UserProducts/Userproducts.styles.jsx
@@ -1,12 +1,62 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
-  margin: 0 auto;
-  width: 1200px;
-  height: ${window.innerHeight - 300}px;
+export const PageContainer = styled.div`
   display: flex;
-  align-items: flex-start;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  min-height: calc(100vh - 388px);
+  width: 0 auto;
+  padding-bottom: 30px;
 `;
 
-export default Container;
+export const PostingsContainer = styled.div`
+  margin: 0 auto;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  column-gap: 20px;
+  row-gap: 30px;
+  margin-bottom: 30px;
+`;
+
+export const PostingContainer = styled.div`
+  width: 285px;
+  padding: 15px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 10px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  row-gap: 15px;
+  transition: transform 0.5s ease;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondaryDark};
+    cursor: pointer;
+    transform: scale(1.05);
+  }
+`;
+
+export const PostingImage = styled.img`
+  width: 255px;
+  height: 255px;
+  object-fit: contain;
+  object-position: center;
+`;
+
+export const TilleContainer = styled.div`
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.heading};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+`;
