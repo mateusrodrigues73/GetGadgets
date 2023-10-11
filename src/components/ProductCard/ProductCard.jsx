764ba32fcb95c66c46b74ea069aff95a954bdeba
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-11 09:57:04 -0300

    feat: acessar página do produto

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,3 +1,5 @@
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import {
@@ -9,24 +11,62 @@ import {
 } from './ProductCard.styles';
 
 import AddToCartButton from '../AddToCartButton';
+import Loader from '../Loader';
 
-const ProductCard = ({ image, title, price }) => {
-  const productPage = () => {};
+import showToast from '../../utils/showToasts';
+
+import { ProductContext } from '../../contexts/ProductProvider';
+
+const ProductCard = ({ image, title, price, postId, sellerId }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const { getPost, getSeller } = useContext(ProductContext);
+
+  const goToProductPage = async () => {
+    setIsLoading(true);
+    const post = await getPost(postId);
+    if (!post) {
+      setIsLoading(false);
+      showToast(
+        `get-post-error-${postId}`,
+        'error',
+        'Ocorreu um erro ao carregar o produto! Tente mais tarde'
+      );
+      return;
+    }
+    const seller = await getSeller(sellerId);
+    if (!seller) {
+      setIsLoading(false);
+      showToast(
+        `get-seller-error-${postId}`,
+        'error',
+        'Ocorreu um erro ao carregar o produto! Tente mais tarde'
+      );
+      return;
+    }
+    setIsLoading(false);
+    navigate(`/produto/${title}/${postId}`);
+  };
+
+  const addToCart = () => {};
 
   return (
-    <ProductContainer>
-      <ProductImage src={image} alt={title} />
-      <TilleContainer>
-        <Title>{title}</Title>
-      </TilleContainer>
-      <Price>{price}</Price>
-      <AddToCartButton
-        width="255px"
-        height="25px"
-        text="Comprar"
-        onClick={productPage}
-      />
-    </ProductContainer>
+    <>
+      <ProductContainer onClick={goToProductPage}>
+        <ProductImage src={image} alt={title} />
+        <TilleContainer>
+          <Title>{title}</Title>
+        </TilleContainer>
+        <Price>{price}</Price>
+        <AddToCartButton
+          width="255px"
+          height="25px"
+          text="Adicionar ao carrinho"
+          onClick={addToCart}
+        />
+      </ProductContainer>
+      {isLoading && <Loader />}
+    </>
   );
 };
 
@@ -34,6 +74,8 @@ ProductCard.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
+  sellerId: PropTypes.string.isRequired,
+  postId: PropTypes.number.isRequired,
 };
 
 export default ProductCard;
