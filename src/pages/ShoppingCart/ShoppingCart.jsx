4e73a57ea072e9a61c398ed24f516c8e6016acdb
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-13 10:31:46 -0300

    feat: mostrar alerta ao adicionar produto no carrinho

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -1,3 +1,20 @@
-const ShoppingCart = () => <div>ShoppingCart</div>;
+import { useEffect, useContext } from 'react';
+
+import showToast from '../../utils/showToasts';
+
+import { ProductContext } from '../../contexts/ProductProvider';
+
+const ShoppingCart = () => {
+  const { postToast, setPostToast } = useContext(ProductContext);
+
+  useEffect(() => {
+    if (postToast) {
+      showToast(postToast.id, postToast.type, postToast.message);
+      setPostToast(null);
+    }
+  }, []);
+
+  return <div>ShoppingCart</div>;
+};
 
 export default ShoppingCart;
