9d708524de2f6786493e8cb0a0770f8c653812a8
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-16 10:51:52 -0300

    feat: adicionar produto no carrinho no card do produto

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -10,30 +11,57 @@ import {
 } from './ProductCard.styles';
 
 import AddToCartButton from '../AddToCartButton';
+import Loader from '../Loader';
+
+import showToast from '../../utils/showToasts';
+
+import { AuthContext } from '../../contexts/AuthProvider';
+import { ProductContext } from '../../contexts/ProductProvider';
 
 const ProductCard = ({ image, title, price, postId }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { sessionUser } = useContext(AuthContext);
+  const { userCartItensIds, addProductToCart } = useContext(ProductContext);
   const navigate = useNavigate();
 
   const goToProductPage = async () => {
     navigate(`/produto/${postId}`);
   };
 
-  const addToCart = () => {};
+  const addToCart = async (event) => {
+    event.stopPropagation();
+    if (!sessionUser) {
+      showToast(
+        `add_product-direct-to-cart-unlogged-warning-${postId}`,
+        'warn',
+        'Você deve estar logado para adicionar produtos no seu carrinho'
+      );
+    } else if (userCartItensIds && !userCartItensIds.includes(postId)) {
+      setIsLoading(true);
+      await addProductToCart(postId);
+      setIsLoading(false);
+    } else {
+      navigate('/carrinho-de-compras');
+    }
+  };
 
   return (
-    <ProductContainer onClick={goToProductPage}>
-      <ProductImage src={image} alt={title} />
-      <TilleContainer>
-        <Title>{title}</Title>
-      </TilleContainer>
-      <Price>{price}</Price>
-      <AddToCartButton
-        width="255px"
-        height="25px"
-        text="Adicionar ao carrinho"
-        onClick={addToCart}
-      />
-    </ProductContainer>
+    <>
+      <ProductContainer onClick={goToProductPage}>
+        <ProductImage src={image} alt={title} />
+        <TilleContainer>
+          <Title>{title}</Title>
+        </TilleContainer>
+        <Price>{price}</Price>
+        <AddToCartButton
+          width="255px"
+          height="25px"
+          text="Adicionar ao carrinho"
+          onClick={addToCart}
+        />
+      </ProductContainer>
+      {isLoading && <Loader />}
+    </>
   );
 };
 
