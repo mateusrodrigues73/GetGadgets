9de95186dec5b51c758bc251877433cefec8020a
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-16 11:00:55 -0300

    styles: adicionar PageContainer

---
+++ src/pages/ShoppingCart/ShoppingCart.styles.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  min-height: 100vh;
+  width: 0 auto;
+  padding-bottom: 30px;
+`;
+
+export default PageContainer;
