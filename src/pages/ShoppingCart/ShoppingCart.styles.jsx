8645c8ad5e5d1c43db9397cd05a4ca4935d43ca8
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-19 20:27:31 -0300

    styles: ajustar responsividade para telas maiores

--- src/pages/ShoppingCart/ShoppingCart.styles.jsx
+++ src/pages/ShoppingCart/ShoppingCart.styles.jsx
@@ -6,7 +6,7 @@ export const PageContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  min-height: 100vh;
+  min-height: calc(100vh - 336px);
   width: 0 auto;
   padding-bottom: 30px;
 `;
