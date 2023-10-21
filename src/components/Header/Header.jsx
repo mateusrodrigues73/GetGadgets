5877c0ee5bd69298a9c0d022ccb0f63a8be3e288
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-10-21 12:31:56 -0300

    feat: adicionar busca de produtos no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,60 +16,73 @@ import {
   LinkWithIcon,
 } from './Header.styles';
 
+import Loader from '../Loader';
+
 import { AuthContext } from '../../contexts/AuthProvider';
+import { ProductContext } from '../../contexts/ProductProvider';
 
 const Header = () => {
-  // eslint-disable-next-line no-unused-vars
   const [searchInput, setSearchinput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const { sessionUser } = useContext(AuthContext);
+  const { getProductsByModel } = useContext(ProductContext);
 
   const handleInputChange = (event) => {
     setSearchinput(event.target.value);
   };
 
-  const findProducts = () => {
-    // TODO: implementar função para busca de produtos
+  const findProducts = async () => {
+    if (!searchInput) {
+      return;
+    }
+    setIsLoading(true);
+    setSearchinput('');
+    await getProductsByModel(searchInput);
+    setIsLoading(false);
   };
 
   return (
-    <HeaderContainer>
-      <HeaderWrapper>
-        <Logo src="/images/logo.png" />
-        <SearchLinksContainer>
-          <SearchContainer>
-            <SearchInput
-              placeholder="busque aqui o seu produto"
-              onChange={handleInputChange}
-            />
-            <SearchIcon onClick={findProducts} />
-          </SearchContainer>
-          <LinksContainer>
-            <LinkItem to="/">Fóruns</LinkItem>
-            <LinkItem to="/">Seu histórico</LinkItem>
-            <LinkItem to="/">Busca avançada</LinkItem>
-            <LinkItem to="/seus-anuncios">Seus anúncios</LinkItem>
-          </LinksContainer>
-        </SearchLinksContainer>
-        <LinkIconContainer>
-          {!sessionUser ? (
-            <LinkWithIcon to="/entrar">
-              Faça seu login <br /> ou cadastre-se
+    <>
+      <HeaderContainer>
+        <HeaderWrapper>
+          <Logo src="/images/logo.png" />
+          <SearchLinksContainer>
+            <SearchContainer>
+              <SearchInput
+                placeholder="busque aqui o seu produto"
+                onChange={handleInputChange}
+                value={searchInput}
+              />
+              <SearchIcon onClick={findProducts} />
+            </SearchContainer>
+            <LinksContainer>
+              <LinkItem to="/">Seu histórico</LinkItem>
+              <LinkItem to="/busca-avancada">Busca avançada</LinkItem>
+              <LinkItem to="/seus-anuncios">Seus anúncios</LinkItem>
+            </LinksContainer>
+          </SearchLinksContainer>
+          <LinkIconContainer>
+            {!sessionUser ? (
+              <LinkWithIcon to="/entrar">
+                Faça seu login <br /> ou cadastre-se
+              </LinkWithIcon>
+            ) : (
+              <LinkWithIcon to="/perfil">
+                Seu perfil de <br /> usuário
+              </LinkWithIcon>
+            )}
+            <UserIcon />
+          </LinkIconContainer>
+          <LinkIconContainer>
+            <LinkWithIcon to="/carrinho-de-compras">
+              Seu carrinho <br /> de compras
             </LinkWithIcon>
-          ) : (
-            <LinkWithIcon to="/perfil">
-              Seu perfil de <br /> usuário
-            </LinkWithIcon>
-          )}
-          <UserIcon />
-        </LinkIconContainer>
-        <LinkIconContainer>
-          <LinkWithIcon to="/carrinho-de-compras">
-            Seu carrinho <br /> de compras
-          </LinkWithIcon>
-          <ShoppingCarIcon />
-        </LinkIconContainer>
-      </HeaderWrapper>
-    </HeaderContainer>
+            <ShoppingCarIcon />
+          </LinkIconContainer>
+        </HeaderWrapper>
+      </HeaderContainer>
+      {isLoading && <Loader />}
+    </>
   );
 };
 
