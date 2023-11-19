4bce5e7dd0106b35001b49f716f8a6be8cad621f
Mateus Rodrigues <mrodrigues7373@gmail.com>
2023-11-19 12:55:44 -0300

    feat: finalizar página do histórico

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,3 +1,366 @@
-export const History = () => <div>History</div>;
+import { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import {
+  PageContainer,
+  HistoricContainer,
+  HistoricWrapper,
+  HistoricTitle,
+  ItenContainer,
+  ItenImage,
+  ItenDataContainer,
+  ItenData,
+  UserContainer,
+  UserTitle,
+  UserWrapper,
+  UserIconWrapper,
+  UserIcon,
+  UserImage,
+  UserDataContainer,
+  StatusContainer,
+  StatusTitle,
+  StatusMessage,
+  HistoricDataContainer,
+  HistoricData,
+} from './History.styles';
+
+import Breadcrumbs from '../../components/Breadcrumbs';
+import SectionTitle from '../../components/SectionTitle';
+import GradientButton from '../../components/GradientButton';
+import Loader from '../../components/Loader';
+import Alert from '../../components/Alert';
+
+import showToast from '../../utils/showToasts';
+
+import { AuthContext } from '../../contexts/AuthProvider';
+import { ProductContext } from '../../contexts/ProductProvider';
+
+export const History = () => {
+  const [isAlerting, setIsAlerting] = useState(false);
+  const [alert, setAlert] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const linksString = '/\\Home';
+  const { sessionUser, setDispathUrl } = useContext(AuthContext);
+  const { historicItems, updateHistoricStatus, postToast, setPostToast } =
+    useContext(ProductContext);
+  const navigate = useNavigate();
+
+  const chatWithSeller = () => {};
+
+  const changeStatus = async (id, status) => {
+    setIsLoading(true);
+    await updateHistoricStatus(id, { status });
+    setIsLoading(false);
+  };
+
+  const goToLogin = () => {
+    setIsAlerting(false);
+    setDispathUrl('/seu-historico');
+    navigate('/entrar');
+  };
+
+  const cancel = () => {
+    setIsAlerting(false);
+  };
+
+  const renderPurchases = (purchases) =>
+    purchases.length > 0 && (
+      <HistoricWrapper>
+        <HistoricTitle>Suas compras</HistoricTitle>
+        {purchases.map((iten) => (
+          <ItenContainer key={iten.id}>
+            <ItenImage src={iten.imagem} alt={iten.titulo} />
+            <ItenDataContainer>
+              <ItenData>
+                {iten.titulo.length > 35
+                  ? `${iten.titulo.substring(0, 32)}...`
+                  : iten.titulo}
+              </ItenData>
+              <ItenData>{`Quantidade: ${iten.quantidade}`}</ItenData>
+              <ItenData>{`Preço: ${iten.quantidade} x R$ ${
+                typeof iten.preco === 'number'
+                  ? iten.preco.toLocaleString('pt-BR', {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })
+                  : iten.preco
+              }`}</ItenData>
+              <ItenData>{`Total: R$ ${(
+                iten.quantidade * iten.preco
+              ).toLocaleString('pt-BR', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}`}</ItenData>
+            </ItenDataContainer>
+            <UserContainer>
+              <UserTitle>Vendedor:</UserTitle>
+              <UserWrapper>
+                <UserIconWrapper>
+                  {iten.vendedor_imagem === 'no_image' ? (
+                    <UserIcon />
+                  ) : (
+                    <UserImage src={iten.vendedor_imagem} alt="Vendedor" />
+                  )}
+                </UserIconWrapper>
+                <UserDataContainer>
+                  <ItenData>{iten.vendedor_nome}</ItenData>
+                  <GradientButton
+                    width="175px"
+                    height="20px"
+                    text="Conversar"
+                    onClick={chatWithSeller}
+                  />
+                </UserDataContainer>
+              </UserWrapper>
+            </UserContainer>
+            {iten.status === 1 ? (
+              <StatusContainer>
+                <StatusTitle>Status: a ser enviado pelo vendedor</StatusTitle>
+              </StatusContainer>
+            ) : (
+              <StatusContainer>
+                <StatusTitle>Status: produto a caminho</StatusTitle>
+                <StatusMessage>
+                  Clique para alterar o status quando o produto chegar
+                </StatusMessage>
+                <GradientButton
+                  width="325px"
+                  height="25px"
+                  text="Alterar Status"
+                  onClick={() => changeStatus(iten.id, 3)}
+                />
+              </StatusContainer>
+            )}
+          </ItenContainer>
+        ))}
+      </HistoricWrapper>
+    );
+
+  const renderSales = (sales) =>
+    sales.length > 0 && (
+      <HistoricWrapper>
+        <HistoricTitle>Suas vendas</HistoricTitle>
+        {sales.map((iten) => (
+          <ItenContainer key={iten.id}>
+            <ItenImage src={iten.imagem} alt={iten.titulo} />
+            <ItenDataContainer>
+              <ItenData>
+                {iten.titulo.length > 35
+                  ? `${iten.titulo.substring(0, 32)}...`
+                  : iten.titulo}
+              </ItenData>
+              <ItenData>{`Quantidade: ${iten.quantidade}`}</ItenData>
+              <ItenData>{`Preço: ${iten.quantidade} x R$ ${
+                typeof iten.preco === 'number'
+                  ? iten.preco.toLocaleString('pt-BR', {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })
+                  : iten.preco
+              }`}</ItenData>
+              <ItenData>{`Total: R$ ${(
+                iten.quantidade * iten.preco
+              ).toLocaleString('pt-BR', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}`}</ItenData>
+            </ItenDataContainer>
+            <UserContainer>
+              <UserTitle>Comprador:</UserTitle>
+              <UserWrapper>
+                <UserIconWrapper>
+                  {iten.comprador_imagem === 'no_image' ? (
+                    <UserIcon />
+                  ) : (
+                    <UserImage src={iten.comprador_imagem} alt="Comprador" />
+                  )}
+                </UserIconWrapper>
+                <UserDataContainer>
+                  <ItenData>{iten.comprador_nome}</ItenData>
+                  <GradientButton
+                    width="175px"
+                    height="20px"
+                    text="Conversar"
+                    onClick={chatWithSeller}
+                  />
+                </UserDataContainer>
+              </UserWrapper>
+            </UserContainer>
+            {iten.status === 2 ? (
+              <StatusContainer>
+                <StatusTitle>Status: produto a caminho</StatusTitle>
+              </StatusContainer>
+            ) : (
+              <StatusContainer>
+                <StatusTitle>Status: a ser enviado</StatusTitle>
+                <StatusMessage>
+                  Clique para alterar o status quando você enviar o produto
+                </StatusMessage>
+                <GradientButton
+                  width="325px"
+                  height="25px"
+                  text="Alterar Status"
+                  onClick={() => changeStatus(iten.id, 2)}
+                />
+              </StatusContainer>
+            )}
+          </ItenContainer>
+        ))}
+      </HistoricWrapper>
+    );
+
+  const renderSellerImage = (iten) =>
+    iten.vendedor_imagem === 'no_image' ? (
+      <UserIcon />
+    ) : (
+      <UserImage src={iten.vendedor_imagem} alt="Vendedor" />
+    );
+
+  const renderBuyerImage = (iten) =>
+    iten.comprador_imagem === 'no_image' ? (
+      <UserIcon />
+    ) : (
+      <UserImage src={iten.comprador_imagem} alt="Comprador" />
+    );
+
+  const formatDate = (date) => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2); // Pegando apenas os dois últimos dígitos do ano
+    return `${day}/${month}/${year}`;
+  };
+
+  const renderHistoricItens = () => {
+    const emAndamento = historicItems.sales
+      .concat(historicItems.purchases)
+      .filter((item) => item.status === 1 || item.status === 2)
+      .sort((a, b) => new Date(b.data) - new Date(a.data));
+    const concluido = historicItems.sales
+      .concat(historicItems.purchases)
+      .filter((item) => item.status === 3)
+      .sort((a, b) => new Date(b.data) - new Date(a.data));
+    const compras = emAndamento.filter(
+      (item) => sessionUser.id === item.id_comprador
+    );
+    const vendas = emAndamento.filter(
+      (item) => sessionUser.id === item.id_vendedor
+    );
+
+    return (
+      <HistoricContainer>
+        {emAndamento.length > 0 && (
+          <>
+            <SectionTitle title="Em andamento" />
+            {renderPurchases(compras)}
+            {renderSales(vendas)}
+          </>
+        )}
+        {concluido.length > 0 && (
+          <>
+            <SectionTitle title="Seu histórico" />
+            <HistoricWrapper>
+              {concluido.map((iten) => (
+                <ItenContainer key={iten.id}>
+                  <ItenImage src={iten.imagem} alt={iten.titulo} />
+                  <ItenDataContainer>
+                    <ItenData>
+                      {iten.titulo.length > 35
+                        ? `${iten.titulo.substring(0, 32)}...`
+                        : iten.titulo}
+                    </ItenData>
+                    <ItenData>{`Quantidade: ${iten.quantidade}`}</ItenData>
+                    <ItenData>{`Preço: ${iten.quantidade} x R$ ${
+                      typeof iten.preco === 'number'
+                        ? iten.preco.toLocaleString('pt-BR', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })
+                        : iten.preco
+                    }`}</ItenData>
+                    <ItenData>{`Total: R$ ${(
+                      iten.quantidade * iten.preco
+                    ).toLocaleString('pt-BR', {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}`}</ItenData>
+                  </ItenDataContainer>
+                  <HistoricDataContainer>
+                    <HistoricData>
+                      {iten.id_comprador === sessionUser.id
+                        ? 'Você: comprou'
+                        : 'Você: vendeu'}
+                    </HistoricData>
+                    <HistoricData>{`Data: ${formatDate(
+                      new Date(iten.data)
+                    )}`}</HistoricData>
+                  </HistoricDataContainer>
+                  <UserContainer>
+                    <UserTitle>
+                      {iten.id_comprador === sessionUser.id
+                        ? 'Vendido por:'
+                        : 'Comprado por:'}
+                    </UserTitle>
+                    <UserWrapper>
+                      <UserIconWrapper>
+                        {iten.id_comprador === sessionUser.id
+                          ? renderSellerImage(iten)
+                          : renderBuyerImage(iten)}
+                      </UserIconWrapper>
+                      <UserDataContainer>
+                        <ItenData>
+                          {iten.id_comprador === sessionUser.id
+                            ? iten.vendedor_nome
+                            : iten.comprador_nome}
+                        </ItenData>
+                      </UserDataContainer>
+                    </UserWrapper>
+                  </UserContainer>
+                </ItenContainer>
+              ))}
+            </HistoricWrapper>
+          </>
+        )}
+      </HistoricContainer>
+    );
+  };
+
+  useEffect(() => {
+    if (!sessionUser) {
+      setAlert(
+        'Você deve estar logado para ver seu histórico. Deseja ir para tela de login?'
+      );
+      setIsAlerting(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (postToast) {
+      showToast(postToast.id, postToast.type, postToast.message);
+      setPostToast(null);
+    }
+  }, []);
+
+  return (
+    <>
+      <Breadcrumbs linksString={linksString} actualPage="Seus anúncios" />
+      {!sessionUser && (
+        <PageContainer>
+          <SectionTitle title="Você deve estar logado para ver seu histórico" />
+        </PageContainer>
+      )}
+      {sessionUser && !historicItems ? (
+        <PageContainer>
+          <SectionTitle title="Você não possui nenhum item no seu histórico" />
+        </PageContainer>
+      ) : (
+        historicItems && <PageContainer>{renderHistoricItens()}</PageContainer>
+      )}
+      {isLoading && <Loader />}
+      {isAlerting && (
+        <Alert message={alert} onCancel={cancel} onContinue={goToLogin} />
+      )}
+    </>
+  );
+};
 
 export default History;
---
+++ src/pages/History/History.styles.jsx
@@ -0,0 +1,165 @@
+import styled from 'styled-components';
+import { UserOutlined } from '@ant-design/icons';
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  min-height: calc(100vh - 336px);
+  width: 0 auto;
+  padding-bottom: 30px;
+`;
+
+export const HistoricContainer = styled.div`
+  margin: 0 auto;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HistoricWrapper = styled.div`
+  width: 1200px;
+  padding: 30px;
+  row-gap: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 20px 0px;
+  margin-bottom: 30px;
+`;
+
+export const HistoricTitle = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.heading};
+  font-size: 24px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+  align-self: flex-start;
+`;
+
+export const ItenContainer = styled.div`
+  width: 1140px;
+  padding: 15px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.secondaryLight};
+  border-radius: 20px 0px;
+`;
+
+export const ItenImage = styled.img`
+  width: 135px;
+  height: 100;
+  object-fit: contain;
+  object-position: center;
+`;
+
+export const ItenDataContainer = styled.div`
+  width: 295px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 5px;
+`;
+
+export const ItenData = styled.p`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+`;
+
+export const UserContainer = styled.div`
+  padding: 5px 15px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 5px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.secondaryDark};
+  border-radius: 10px 0px;
+`;
+
+export const UserTitle = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.heading};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+  align-self: flex-start;
+`;
+
+export const UserWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const UserIconWrapper = styled.div`
+  width: 60px;
+  height: 60px;
+  background-color: ${({ theme }) => theme.colors.neutralWith};
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const UserIcon = styled(UserOutlined)`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.secondaryLight};
+  margin-bottom: 7px;
+`;
+
+export const UserImage = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50px;
+`;
+
+export const UserDataContainer = styled.div`
+  height: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const StatusContainer = styled.div`
+  width: 325px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 7px;
+`;
+
+export const StatusTitle = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+  text-align: center;
+`;
+
+export const StatusMessage = styled.p`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+  text-align: center;
+`;
+
+export const HistoricDataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 15px;
+`;
+
+export const HistoricData = styled.p`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.neutralWith};
+`;
